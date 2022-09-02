5120c9ba82f19317a23ef41533c4ca0f79b06acd
matheustkaczyk <matheus111904@hotmail.com>
2022-09-02 14:18:46 -0300

    feat: creating xpath dict to id page elements

---
+++ index.py
@@ -0,0 +1,5 @@
+indexes = {
+  'login_xpath': '//*[@id="cpfusuario"]',
+  'password_xpath': '/html/body/div[2]/form[1]/div[3]/div/input',
+  'submit_login': '/html/body/div[2]/form[1]/div[4]/button',
+}
