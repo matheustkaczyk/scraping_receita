17e49d8b53e1fd971f2b122eceabff32cabfc3fa
matheustkaczyk <matheus111904@hotmail.com>
2022-09-02 14:39:54 -0300

    feat: adding consulting page indexes, capturing indexes and clicking

--- index.py
+++ index.py
@@ -1,5 +1,15 @@
-indexes = {
+login_index = {
   'login_xpath': '//*[@id="cpfusuario"]',
   'password_xpath': '/html/body/div[2]/form[1]/div[3]/div/input',
   'submit_login': '/html/body/div[2]/form[1]/div[4]/button',
+}
+
+main_index = {
+  'nf_wrapper': '//*[@id="menulateral"]/div/a[15]',
+  'nf_consulting': '//*[@id="menuLink1206"]'
+}
+
+consulting_index = {
+  'consulting_select': '//*[@id="app"]/div[1]/article/div[2]/div[1]/div[1]/div/select',
+  'consulting_filter_select': '//*[@id="app"]/div[1]/article/div[2]/div[2]/div[1]/div/select'
 }
--- main.py
+++ main.py
@@ -2,16 +2,38 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
 import os
+import time
 
-from index import indexes
+from index import (
+  login_index,
+  main_index
+)
 
 load_dotenv()
 
 CPF  = os.environ.get('CPF')
 PASSWORD = os.environ.get('PASSWORD')
-
 URL='https://receita.pr.gov.br/login'
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
 driver.get(URL)
+
+# Login
+login_input = driver.find_element_by_xpath(login_index['login_xpath'])
+login_password = driver.find_element_by_xpath(login_index['password_xpath'])
+login_submit = driver.find_element_by_xpath(login_index['submit_login'])
+
+login_input.send_keys(CPF)
+login_password.send_keys(PASSWORD)
+login_submit.click()
+
+time.sleep(2)
+
+# Logado, tela principal
+main_wrapper = driver.find_element_by_xpath(main_index['nf_wrapper'])
+main_wrapper.click()
+
+main_consulting = driver.find_element_by_xpath(main_index['nf_consulting'])
+main_consulting.click()
+
+time.sleep(30)
