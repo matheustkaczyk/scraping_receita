83011d738e5a01d7d45d78d77d1615fffbda6ad9
matheustkaczyk <matheus111904@hotmail.com>
2022-09-02 14:22:21 -0300

    feat: capturing ambient variables

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
+import os
+
+from index import indexes
 
 load_dotenv()
+
+CPF  = os.environ.get('CPF')
+PASSWORD = os.environ.get('PASSWORD')
 
 URL='https://receita.pr.gov.br/login'
 
