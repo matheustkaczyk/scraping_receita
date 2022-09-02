8604436e9f63e170923ce42d464cc9669c2c9671
matheustkaczyk <matheus111904@hotmail.com>
2022-09-02 14:15:07 -0300

    feat: preparing to use dotenv

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL='https://receita.pr.gov.br/login'
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+driver.get(URL)
