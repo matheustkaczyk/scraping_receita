7685cd1be89cd5ba2ea1b687fc81c787724bc69c
matheustkaczyk <matheus111904@hotmail.com>
2022-10-18 10:30:12 -0300

    refact: removendo codigo comentado, foi refatorado

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from seleniumwire import webdriver as webdriver_seleniumwire
 from dotenv import load_dotenv
 from datetime import datetime
+from pandas import pd
 import os
 import time
 import datetime
@@ -97,50 +98,6 @@ driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 #Captura dos dados
 partial_data = []
-# list_table_elements = driver.find_elements(By.XPATH, data_index['data_table'])
-
-# line_number = 1
-# page_number = 3 # Página 3 é a primeira página com dados
-# page_list_xpath = f'//*[@id="app"]/div[1]/div/div/div[2]/table/tfoot/tr/td/div/nav/ul/li[{page_number}]'
-
-# for element in list_table_elements:
-#   element_text = element.text + ' '
-#   split_element = element_text.split(' ')
-#   splitted_date = split_element[1].split('/')
-#   formatted_date = [int(splitted_date[2]), int(splitted_date[1]), int(splitted_date[0])]
-#   element_date = datetime.date(year=formatted_date[0], month=formatted_date[1], day=formatted_date[2])
-#   before_folder = os.listdir(os.getcwd())
-
-#   if(element_date <= date_target_parsed):
-#     xml_btn_path = f'//*[@id="app"]/div[1]/div/div/div[2]/table/tbody/tr[{line_number}]/td[10]/button'
-    
-#     xml_btn = driver.find_element(By.XPATH, xml_btn_path)
-#     xml_btn.click()
-
-#     time.sleep(2.5)
-
-#     after_folder = os.listdir(os.getcwd())
-#     diff = set(after_folder) - set(before_folder)
-
-#     with open('nfae.xml', 'rb') as xml_file:
-#       xml_data = xmltodict.parse(xml_file.read())
-#       total_value = xml_data['nfeProc']['NFe']['infNFe']['total']['ICMSTot']['vNF']
-
-#     if(len(diff) == 1):
-#       file_name = diff.pop()
-#       os.remove(file_name)
-#     else:
-#       print('More than one file or no file downloaded')
-
-#     line_number += 1
-
-#     final_data.append({
-#       'date': split_element[1],
-#       'code': split_element[2],
-#       'value': total_value
-#     })
-  
-# print(final_data)
 
 for requests in driver.requests:
   if (requests.url == 'https://nfae.fazenda.pr.gov.br/nfae/api/nfae?acao=LISTAR'):
@@ -150,7 +107,6 @@ time.sleep(2)
 
 parsing = json.loads(partial_data[0])
 final_json = json.dumps(parsing)
-
 
 json_data = json.loads(final_json)
 
@@ -166,5 +122,9 @@ for item in json_data['lista']:
     })
 
 driver.quit()
+
+## Resultado
+df = pd.DataFrame(final_data)
+df.to_excel('dados.xlsx', index=False)
 
 time.sleep(30)
